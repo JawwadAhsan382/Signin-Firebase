5640b3d1929ee331495ab2648da36a6e9fb2f824
JawwadAhsan382 <jawwad654321@gmail.com>
2025-03-14 20:25:23 +0500

    styled

---
+++ app.js
@@ -0,0 +1,20 @@
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAamAE8werhSbWqv1eSqRyOkBgy_OHQ1hc",
+//   authDomain: "pro1-f0a5a.firebaseapp.com",
+//   projectId: "pro1-f0a5a",
+//   storageBucket: "pro1-f0a5a.firebasestorage.app",
+//   messagingSenderId: "987852245108",
+//   appId: "1:987852245108:web:d6d42b03b39c3d930ee921"
+// };
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     const user = userCredential.user;
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
